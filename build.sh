cbcbd759a3f95ba9f8f67227bc86e6ea65f3b948
Tiryoh <tiryoh@gmail.com>
2019-04-29 02:40:49 +0900

    Update ci log

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eu
+set -eux
 
 SRC_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
@@ -8,9 +8,9 @@ cp /usr/bin/qemu-aarch64-static $SRC_DIR/arm64
 cd $SRC_DIR/amd64 && docker build -t ros2:x64-$TARGET .
 cd $SRC_DIR/arm64 && docker build -t ros2:arm64v8-$TARGET .
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ ! $(echo $TARGET | grep 'test') ]]; then echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin; fi
-docker images
 docker tag ros2:x64-$TARGET $DOCKER_USERNAME/ros2:x64-$TARGET
 docker tag ros2:arm64v8-$TARGET $DOCKER_USERNAME/ros2:arm64v8-$TARGET
+docker images
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ ! $(echo $TARGET | grep 'test') ]]; then docker push $DOCKER_USERNAME/ros2:arm64v8-$TARGET; fi
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ ! $(echo $TARGET | grep 'test') ]]; then docker push $DOCKER_USERNAME/ros2:x64-$TARGET; fi
 docker manifest create --amend $DOCKER_USERNAME/ros2:$TARGET $DOCKER_USERNAME/ros2:arm64v8-$TARGET $DOCKER_USERNAME/ros2:x64-$TARGET
