2bcf8cbebc92531fe05e0a7c3fb7d3c010baf078
Tiryoh <tiryoh@gmail.com>
2019-04-29 17:01:33 +0900

    Fix docker manifest error

--- build.sh
+++ build.sh
@@ -7,12 +7,14 @@ sudo apt-get install -y qemu-user-static
 cp /usr/bin/qemu-aarch64-static $SRC_DIR/arm64
 cd $SRC_DIR/amd64 && docker build -t ros2:x64-$TARGET .
 cd $SRC_DIR/arm64 && docker build -t ros2:arm64v8-$TARGET .
-if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ ! $(echo $TARGET | grep 'test') ]]; then echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin; fi
 docker tag ros2:x64-$TARGET $DOCKER_USERNAME/ros2:x64-$TARGET
 docker tag ros2:arm64v8-$TARGET $DOCKER_USERNAME/ros2:arm64v8-$TARGET
 docker images
-if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ ! $(echo $TARGET | grep 'test') ]]; then docker push $DOCKER_USERNAME/ros2:arm64v8-$TARGET; fi
-if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ ! $(echo $TARGET | grep 'test') ]]; then docker push $DOCKER_USERNAME/ros2:x64-$TARGET; fi
-docker manifest create --amend $DOCKER_USERNAME/ros2:$TARGET $DOCKER_USERNAME/ros2:arm64v8-$TARGET $DOCKER_USERNAME/ros2:x64-$TARGET
-docker manifest annotate $DOCKER_USERNAME/ros2:$TARGET $DOCKER_USERNAME/ros2:arm64v8-$TARGET --arch arm64 
-if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ ! $(echo $TARGET | grep 'test') ]]; then docker manifest push $DOCKER_USERNAME/ros2:$TARGET; fi
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ ! $(echo $TARGET | grep 'test') ]]; then
+	echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+	docker push $DOCKER_USERNAME/ros2:arm64v8-$TARGET
+	docker push $DOCKER_USERNAME/ros2:x64-$TARGET
+	docker manifest create --amend $DOCKER_USERNAME/ros2:$TARGET $DOCKER_USERNAME/ros2:arm64v8-$TARGET $DOCKER_USERNAME/ros2:x64-$TARGET
+	docker manifest annotate $DOCKER_USERNAME/ros2:$TARGET $DOCKER_USERNAME/ros2:arm64v8-$TARGET --arch arm64 
+	docker manifest push $DOCKER_USERNAME/ros2:$TARGET
+fi
