3cba91cde8a7e474e2b1330f9ce9dd9f3e016afb
Daisuke Sato <tiryoh@gmail.com>
2025-10-25 02:36:35 +0900

    feat: Add ROS 2 Rolling Dockerfile

---
+++ rolling/ros_entrypoint.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+set -e
+
+DEFAULT_USER=${DEFAULT_USER:-'ubuntu'}
+DEFAULT_USER_UID=${USER_UID:-'1000'}
+DEFAULT_USER_GID=${USER_GID:-'1000'}
+NOPASSWD=${NOPASSWD:-''} # set 'NOPASSWD:' to disable asking sudo password
+SHELL=${DEFAULT_SHELL:-$SHELL}
+
+if [[ $(id -u) -eq 0 ]]; then
+	echo "Set disable_coredump false" >> /etc/sudo.conf
+else
+	echo "Set disable_coredump false" | sudo tee -a /etc/sudo.conf > /dev/null
+fi
+
+# override if $USER env exists
+if [[ ! -z "$USER" ]]; then
+	DEFAULT_USER=$USER
+fi
+
+# create development user
+if [[ $(id -u) -eq 0 ]]; then
+	EXEC="exec /sbin/su-exec ${DEFAULT_USER}"
+
+	# if the user does not exist, create user
+	if [[ $(id -u ${DEFAULT_USER} 2> /dev/null) != ${DEFAULT_USER_UID} ]]; then
+		echo creating user ${DEFAULT_USER}
+		groupadd -g "${DEFAULT_USER_GID}" "${DEFAULT_USER}"
+		useradd --create-home --home-dir /home/${DEFAULT_USER} --uid ${DEFAULT_USER_UID} --shell /bin/bash \
+		    --gid ${DEFAULT_USER_GID} --groups sudo ${DEFAULT_USER}
+		echo "${DEFAULT_USER}:${DEFAULT_USER}" | chpasswd && \
+		echo "${DEFAULT_USER} ALL=(ALL) ${NOPASSWD}ALL" >> /etc/sudoers
+		touch /home/${DEFAULT_USER}/.sudo_as_admin_successful
+	fi
+
+	# mount develop workspace
+	if [ -e /ws ]; then
+		mkdir -p /home/${DEFAULT_USER}/ros2_ws
+		ln -s /ws /home/${DEFAULT_USER}/ros2_ws/src
+	else
+		mkdir -p /home/${DEFAULT_USER}/ros2_ws/src
+	fi
+
+	# setup ros environment
+	echo 'source /opt/ros/${ROS_DISTRO}/setup.bash' >> /home/${DEFAULT_USER}/.bashrc
+	chown -R ${DEFAULT_USER}:${DEFAULT_USER} /home/${DEFAULT_USER}
+
+	DEFAULT_USER_UID="$(${EXEC} id -u)"
+	DEFAULT_USER_GID="$(${EXEC} id -g)"
+	HOME="/home/${DEFAULT_USER}"
+else # use existing user
+	EXEC="exec"
+	DEFAULT_USER="$(whoami)"
+	DEFAULT_USER_UID="$(id -u)"
+	DEFAULT_USER_GID="$(id -g)"
+	if [[ ! -e /home/${DEFAULT_USER}/.bashrc ]]; then
+		cp /etc/skel/.* /home/$DEFAULT_USER/
+	fi
+fi
+
+echo "Launched container with user: $DEFAULT_USER, uid: $DEFAULT_USER_UID, gid: $DEFAULT_USER_GID"
+
+cd $HOME
+
+if which "$1" > /dev/null 2>&1 ; then
+	source /opt/ros/${ROS_DISTRO}/setup.bash
+	$EXEC "$@"
+else
+	echo $@ | $EXEC $SHELL -li
+fi
